785c0464084dd9bc6eb02a2278092de7262c6290
Judith <judith.ovando.l@gmail.com>
2023-05-26 18:12:51 -0400

    login funcionando solo falta dar el alert

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login';
+import Alert from './Components/Alert';
 
 function App() {
+
+  const testLogin = (user) => {
+    const validEmail = "usuario@email.com"
+    const validPass = "password"
+    //console.log(validEmail,validPass)
+    if(user.email === validEmail && user.password === validPass )
+    {
+      //<Alert />
+      alert("exitoso")
+    }
+    else
+    {
+      <Alert />
+      alert("eeenngh")
+    }
+  }
+
   return (
     <div className="App">
      
-       <div class='container p-3'>
-          <header class="h1">Login con React</header>
+       <div className='container p-3'>
+          <header className="h1">Login con React</header>
 
-         <div class='border p-3'>
-            <form>
-              <div class="text-start">
-                  <label for="exampleInputEmail1" class="form-label mb-2 m-2">Email address</label>
-                    <input type="email" placeholder="Escriba aquí su correo" class="form-control m-2" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                   <div class="mb-3">
-                     <label for="exampleInputPassword1" class="form-label m-2">Password</label>
-                       <input type="password" class="form-control m-2" id="exampleInputPassword1" placeholder="Escriba aquí su contraseña"/>
-                   </div>
-                  
-              </div>
-                   <div class="mb-4">
-                    <button type="submit" class="btn btn-primary">Validar</button>
-                   </div>
-           </form>
+         <div className='border p-3'>
+          <Login onSubmit={testLogin}/>
+            
           </div>
        </div>
     </div>
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,11 +3,37 @@
 //  incluir un botón para procesar el formulario, el botón debe estar deshabilitado mientras los campos email y contraseña estén vacíos. (1 Punto)
 // ○ Puedes utilizar disabled y validar los campos con trim().
 // El botón debe validar mediante el evento onSubmit si el email y la contraseña son correctos
+import React, { useState } from "react";
 
+const Login = ( {onSubmit}) => {
 
-const login = ({email,password}) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const valid = (e) => {
+     // e.preventDefaul() -- esto mareaba la ejecución
+      console.log(email,password)
+      onSubmit({email,password})
+      
+    }
+    return (
+            
+          <form onSubmit={valid}>
+              <div className="text-start">
+                 <label className="form-label mb-2 m-2"  htmlFor="email">Correo electrónico</label>
+                <input type="email" placeholder="Escriba aquí su correo" className="form-control m-2" htmlFor="email"  id="email"  onChange={(e)=>setEmail(e.target.value)}/>
+                </div>
+               <div className="mb-3">
+                 <label className="form-label m-2" htmlFor="password">Contraseña</label>
+                   <input type="password" className="form-control m-2"  htmlFor="password" id="password" placeholder="Escriba aquí su contraseña" onChange={(p)=>setPassword(p.target.value)}/>
+               </div>
+                   <div className="mb-4">
+                     <button type="submit" className="btn btn-primary" disabled={!email.trim() || !password.trim()}>Validar</button>
+                  </div>
+               </form>
+            )
+    ;
     
-
 };
 
-export default login;
+export default Login;
